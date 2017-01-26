1a9b6fe27ef7e0a9da6390dc5b84175782c9d594
dramanebamba <dramane.bamba@gmail.com>
2017-01-26 02:43:35 +0100

    issue1

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ var app = express();
 //Définition du dossier public
 app.use(express.static('public'));
 
-https.createServer({ 
-        key: fs.readFileSync("/etc/letsencrypt/archive/dramane.fr/privkey1.pem"),
-        cert: fs.readFileSync("/etc/letsencrypt/archive/dramane.fr/fullchain1.pem"),
-        ca: fs.readFileSync("/etc/letsencrypt/archive/dramane.fr/chain1.pem")
-}, app).listen(443);
+// https.createServer({
+//         key: fs.readFileSync("/etc/letsencrypt/archive/dramane.fr/privkey1.pem"),
+//         cert: fs.readFileSync("/etc/letsencrypt/archive/dramane.fr/fullchain1.pem"),
+//         ca: fs.readFileSync("/etc/letsencrypt/archive/dramane.fr/chain1.pem")
+// }, app).listen(443);
 
 /* On affiche la todolist et le formulaire */
 app.get('/', function(req, res) {
-    res.render('index.ejs');
+    res.render('pages/index.ejs');
 })
 
 app.get('/responsive', function(req, res) {
-    res.render('responsive.ejs');
+    res.render('pages/responsive.ejs');
 })
 
 app.get('/backgrounds', function(req, res) {
-    res.render('backgrounds.ejs');
+    res.render('pages/backgrounds.ejs');
 })
 
 /* On redirige vers la todolist si la page demandée n'est pas trouvée */
